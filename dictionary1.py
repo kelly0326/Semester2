7dbfb9519ebb3940a70d25bd65e235d1786fbc59
shu liu <shu@liu.net>
2024-11-08 21:26:20 -0800

    Add another file about dictionary as a test by Shu

---
+++ dictionary1.py
@@ -0,0 +1,23 @@
+# Dictionary
+# testing example
+
+info = {'personal_data':
+         {'name': 'Hermione',
+          'age': 16,
+          'major': 'Potions',
+          'physical_features':
+             {'color': {'eye': 'brown',
+                        'hair': 'brown'},
+              'height': "5'6"}
+         },
+       'other':
+         {'friends': ['Harry', 'Ron', 'Victor'],
+          'interests': ['horcruxes', 'homework', 'dentistry']
+         }
+      }
+
+info.keys()
+# dict_keys(['personal_data', 'other'])
+
+info["personal_data"]["physical_features"]["color"]["hair"]
+# 'brown'
