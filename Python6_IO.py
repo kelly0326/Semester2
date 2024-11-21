f47e17448e6487711d9f2a8bd4b43ac335eb0459
shu liu <shu@liu.net>
2024-11-21 12:24:53 -0800

    Edit the Python6_IO.py file by Shu 11/21/2024

--- Python6_IO.py
+++ Python6_IO.py
@@ -51,3 +51,28 @@ while True:
         break
     else:
         print("your value:", value, "is not a positive number")
+
+deposit = input("how much is your deposit? ")
+deposit = float(deposit)
+
+interest_rate = 3.5 / 100
+year = input("how long do you want to deposit? ")
+year = int(year)
+print(deposit, year)
+
+balance = deposit
+for i in range(year):
+    balance = balance * (1 + interest_rate)
+    print(f"year {i + 1:>2d}: your balance is ${balance:,.2f}")
+
+balance = deposit
+count = 0
+while balance < deposit * 2:
+    balance = balance * (1 + interest_rate)
+    count = count + 1
+
+print(count, balance)
+print("-" * 50)
+print("it will take", count, "years to double the deposit.")
+print("after that, deposit will be: ", balance)
+print("-" * 50)
