910a3b3fa73a10cc41a623f36d36e000536fc9fd
shu liu <shu@liu.net>
2024-12-01 18:58:28 -0800

    Edit the test.py file 12/1/2024 by Shu

--- test.py
+++ test.py
@@ -4,3 +4,19 @@ def plus(a, b):
 print(plus(3, 5))
 plus(10, 20)
 print("program runs successfully.")
+
+
+def test(a, b, c):
+	return (a + b + c)
+
+test(1, 5, 2.5)
+
+result = test(1, 5, 2.5)
+print(result)
+
+
+print("==================")
+def test(a, b):
+	return (a * b)
+
+print(test(3, 9))
