bc67701c501bd1c2ad6aed011419f289cfefc962
shu liu <shu@liu.net>
2024-12-03 14:02:59 -0800

    Edit the file with #(result) by Shu 12/3/2024

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 def plus(a, b):
 	print("in function", a + b)
 	return (a + b)
+
 print(plus(3, 5)) # 8 (why not 'in function', 8?)
-plus(10, 20)
+plus(10, 20) # in function, 30
 print("program runs successfully.")
 
 
@@ -14,7 +15,7 @@ def test(a, b, c):
 test(1, 5, 2.5)
 
 result = test(1, 5, 2.5)
-print(result)
+print(result) # 8.5
 
 
 print("==================")
