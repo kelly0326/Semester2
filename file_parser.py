1b7768e25d213f7828c70929e3315ba1c1d615fe
shu liu <shu@liu.net>
2024-12-16 09:18:27 -0800

    Edit the file_parser file by Shu 12/16/2024

--- file_parser.py
+++ file_parser.py
@@ -14,7 +14,7 @@ def convert_type(lst):
         if number != 0:
             count.append(float(lst[number]))
     print(count)
-
+    return count
     '''
     # Doc string
     Description: 
@@ -33,10 +33,36 @@ def get_column(data, column_number):
     lst = []
     for row in data:
         lst.append(row[column_number])
+    print("in get_column", lst)
     return lst
-
+    """
+    Description:
+        This function takes two parameters data and column_number
+        and `append` the element into a new empty list(lst).
+    Parameter: data, column_number
+    Return: lst
+    Precondition: data must be a 2D list, column_number must be an integar
+                  smaller than the length of the first row
+    """
 
 def read_data(path):
+    file = open(path)
+    data = file.read()
+    print(repr(data)) # return print (to see raw string)
+    print(data)
+    print("50",type(data)) # str
+
+    data1 = data.split("\n")
+    print("53",type(data1)) # list
+    print(data1)
+
+    data2 = []
+    for row in data1:
+        temp = row.split(",")
+        print("temp=", temp)
+        data2.append(temp)
+    print(data2)
+    return data2
     """
     Description:
         This function takes file name as path, `open`the file
@@ -48,16 +74,19 @@ def read_data(path):
     Precondition: 
         File must in the same folder with Python script, file name must correct
     """
-    file = open(path)
-    data = file.read()
-    data1 = data.split("\n")
-    data2 = []
-    for row in data1:
-        temp = row.split(",")
-        print(temp)
-        data2.append(temp)
-    return data2
 
+def get_avg(lst):
+    """
+    Description:
+        This function takes a list and return the average of this list.
+    Parameter: lst(list)
+    Return: float
+    Precondition:
+        List must be 1D python list.
+        List element data type must be integar or float.
+    """
+    total = sum(lst)
+    return total / len(lst)
 
 def main():
     # Define the path to the file
@@ -65,14 +94,21 @@ def main():
 
     # call read_data to get the 2d list
     data2 = read_data(path)
-    print(data2)
 
     # use the previous data and get_column to retrieve the column
-    get_column(data, column_number)
+    my_column = get_column(data2, 4)
+    print("in main", my_column)
 
     # convert the column to float
-    convert_type(lst)
+    float_column = convert_type(my_column)
+    print("in main", float_column)
+
+    # print average
+    average = get_avg(float_column)
+    print(average)
 
     print("System exit successfully.")
-
-main()
+print("in file parser",__name__)   # run first
+if __name__ == '__main__':
+    main()
+# main() # run second
